85842abc3067092d45d610feaf915ad2d7ee4196
AmshenShanu07 <amshen.shanu07.ya@gmail.com>
2021-07-19 16:34:21 +0530

    tool vital updation

--- HackPhiles/insta.py
+++ HackPhiles/insta.py
@@ -1,11 +1,5 @@
 import time
-import json
-import random
-import string
-import os, sys
-import requests
-import collections
-import urllib.request
+import sys
 from bs4 import BeautifulSoup
 
 nu = '\033[0m'
@@ -19,7 +13,7 @@ def write(in_text):
 		sys.stdout.write(char)
 		sys.stdout.flush()
 
-write(f"{gr}[+]{mg}Coded by @Green_Hat{nu}....")
+write(f"{gr}[+]{mg}Coded by AmshenShanu07{nu}....")
 print(f"{cy}\n------------------------")
 time.sleep(1)
 write(f"{gr}[+]{re}Starting Instagram Bruteforce{nu}...")
--- HackPhiles/installing.py
+++ HackPhiles/installing.py
@@ -1,11 +1,5 @@
 import time
-import json
-import random
-import string
-import os, sys
-import requests
-import collections
-import urllib.request
+import sys
 from bs4 import BeautifulSoup
 
 nu = '\033[0m'
@@ -19,10 +13,10 @@ def write(in_text):
 		sys.stdout.write(char)
 		sys.stdout.flush()
 
-write(f"{gr}[+]{mg}Coded by @Green_Hat{nu}....")
+write(f"{gr}[+]{mg}Coded by AmshenShanu07{nu}....")
 print(f"{cy}\n------------------------")
 time.sleep(1)
-write(f"{gr}[+]{re} Thanks to HackPhiles community{nu}...")
+write(f"{gr}[+]{re} From The Hearts HackPhiles community{nu}...")
 print(f"\n-----------------------------")
 write(f"\n{gr}[+]{mg}Start your tool by ./run  {nu} ")
 print("\n")
